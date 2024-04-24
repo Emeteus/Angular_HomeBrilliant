76e533a7cf2a2deebd16902d0c6fcff9e2262de3
Emeteus <abodemaxmod2002@gmail.com>
2024-04-24 19:21:02 +0300

    bug fix

--- src/app/pages/profile/change-profile/change-profile.component.ts
+++ src/app/pages/profile/change-profile/change-profile.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-change-profile',
   templateUrl: './change-profile.component.html',
   styleUrl: './change-profile.component.css'
 })
-export class ChangeProfileComponent {
+export class ChangeProfileComponent implements OnInit {
   ngOnInit(): void {
-    document.title = 'Change Profile | BrilliantHome'; // Установка нового названия вкладки
+    document.title = 'Change Profile | BrilliantHome'; // Set document title
   }
 
+  imageElement(event: any): void {
+    const image: HTMLImageElement | null = document.querySelector(".img-account-profile");
+
+    if (image) {
+      const file: File = event.target.files[0];
+      const reader = new FileReader();
+
+      reader.onload = (e: any) => {
+        if (typeof e.target.result === 'string') {
+          image.src = e.target.result;
+        }
+      };
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+  }
 }
